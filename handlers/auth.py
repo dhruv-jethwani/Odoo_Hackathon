95a4e88dd8a665f931532d748838d75949df59b1
Dhruv Jethwani <dhruvjethwani56@gmail.com>
2025-10-04 10:39:21 +0530

    hide navbar during register and login

--- handlers/auth.py
+++ handlers/auth.py
@@ -9,7 +9,7 @@ from flask import redirect
 @auth_bp.route("/register", methods=["GET", "POST"])
 def register():
     if request.method == "GET":
-        return render_template("register.html")
+        return render_template("register.html", hide_navbar=True)
 
     # Get form data
     username = request.form.get("username", "").strip()
@@ -18,10 +18,10 @@ def register():
 
     # Validation
     if not username or not email or not password:
-        return render_template("register.html", error_msg="⚠️ All fields are required")
+        return render_template("register.html", error_msg="⚠️ All fields are required", hide_navbar=True)
 
     if users.is_email_already_taken(email):
-        return render_template("register.html", error_msg="⚠️ Email already registered")
+        return render_template("register.html", error_msg="⚠️ Email already registered", hide_navbar=True)
 
     # Hash password
     hashed_password = generate_password_hash(password)
@@ -43,19 +43,19 @@ def register():
 @auth_bp.route("/login", methods=["GET", "POST"])
 def login():
     if request.method == "GET":
-        return render_template("login.html")
+        return render_template("login.html", hide_navbar=True)
 
     # Get form data
     email = request.form.get("email", "").strip()
     password = request.form.get("password", "")
     print(email,password)
     if not email or not password:
-        return render_template("login.html", error_msg="⚠️ All fields are required")
+        return render_template("login.html", error_msg="⚠️ All fields are required", hide_navbar=True)
 
     # Get user from DB
     user = users.get_user_by_email(email)
     if not user or not check_password_hash(user.password, password):
-        return render_template("login.html", error_msg="❌ Invalid credentials")
+        return render_template("login.html", error_msg="❌ Invalid credentials", hide_navbar=True)
 
     # Update session token (only needs email, not password)
     users.update_session_token(email)
